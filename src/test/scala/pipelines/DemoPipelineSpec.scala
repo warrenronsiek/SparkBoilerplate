6e1d18f867f1c46d85b7050c2ed19794d2dab2a7
warrenronsiek <warren@warren-r.com>
2019-10-20 16:25:16 -0700

    added simple deequ validation

--- src/test/scala/pipelines/DemoPipelineSpec.scala
+++ src/test/scala/pipelines/DemoPipelineSpec.scala
@@ -3,8 +3,11 @@ package pipelines
 import org.apache.spark.ml.PipelineModel
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Suite}
+import com.amazon.deequ.VerificationSuite
+import com.amazon.deequ.checks.{Check, CheckLevel, CheckStatus}
 
-trait SparkContextMixin extends BeforeAndAfterAll {this: Suite =>
+trait SparkContextMixin extends BeforeAndAfterAll {
+  this: Suite =>
   val spark: SparkSession = SparkSession
     .builder
     .master("local")
@@ -21,7 +24,19 @@ class DemoPipelineSpec extends FlatSpec with SparkContextMixin {
   val demoPipeline = new DemoPipeline(getClass.getResource("/iris.csv").getPath)
   val result: DataFrame = demoPipeline.run()
 
-  "iris dataframe" should "not have Iris-setosa" in {
+  "iris dataframe" should "have 100 rows" in {
     assert(result.count() == 100)
+  }
+
+  it should "pass deequ validation" in {
+    val verificationSuite = VerificationSuite().onData(result)
+      .addCheck(Check(CheckLevel.Error, "simple iris tests")
+          .hasSize(_ == 100)
+          .isComplete("sepal_width")
+          .isComplete("species")
+          .isContainedIn("species", Array("Iris-versicolor", "Iris-virginica"))
+      ).run()
+
+    assert(verificationSuite.status == CheckStatus.Success)
   }
 }
